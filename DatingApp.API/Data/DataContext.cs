839079605116b931076a45be4bc2c3cb9791e3cb
Aleksa Cebedzic <aleksa@seriecom.net>
2018-12-17 14:38:04 +0100

    finished messages

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -5,11 +5,13 @@ namespace DatingApp.API.Data
 {
     public class DataContext:DbContext
     {
-         public DataContext(DbContextOptions<DataContext> options):base(options){}
-         public DbSet<Value> Values { get; set; }
-         public DbSet<User> Users { get; set; }
-         public DbSet<Photo> Photos { get; set; }
-         public DbSet<Like> Likes { get; set; }
+        public DataContext(DbContextOptions<DataContext> options):base(options){}
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Like>()
@@ -24,6 +26,16 @@ namespace DatingApp.API.Data
                 .HasOne(u => u.Liker)
                 .WithMany(u => u.Likees)
                 .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessageReceived)
                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
